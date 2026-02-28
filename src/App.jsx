d5c04ba69ca81ca4151c5e9700115e8a3ac174dc
Laxmipriya-1 <150228943+Laxmipriya-1@users.noreply.github.com>
2026-02-28 21:48:30 +0530

    Front Page Design  and API KEY Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import WeatherBG from './Component/WeatherBG'
 
 const App = () => {
+
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('');
+  const [suggestion , setSuggestion] = useState([]);
+
+
+  const API_KEY = 'b92c59f270b6b170e9cd45c8c04d56ae'
+  //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  //https://api.openweathermap.org/data/2.5/weather?lat=${s.lat}&lon=${s.lon}&appid={API_KEY}&lang={lang}
+   
+
+  //this function check weather exists and return an object
+  const getWeatherCondition = () => weather && ({
+    main: weather.weather[0].main,
+    isDay : Date.now() /1000 > weather.sys.sunrise &&  Date.now() /1000 < weather.sys.sunset
+  })
+
   return (
-    <div className="text-red-500 text-3xl font-bold underline">App</div>
+    <div className=" min-h-screen">
+      <WeatherBG condition={getWeatherCondition()} />
+     
+     <div className='flex items-center justify-center p-6 min-h-screen'>
+      <div className='bg-transparent backdrop-filter backdrop-blur-md rounded-xl
+      shadow-2xl p-8 max-w-md text-white  w-full border border-white/30 relative z-10 '> 
+        <h1 className='text-4xl font-extrabold text-center mb-6'>
+          Weather Application
+        </h1>
+
+        {weather ? (
+          <form onSubmit={handleSearch} className='flex flex-col relative'>
+            <input value={{city}} onChange={(e) => setCity(e.target.value)} placeholder='Enter City or Country (min 3 letters)'
+              className='mb-4 p-3 rounded border border-sky-800 bg-transparent text-white placeholder:white
+              focus:outline-none focus:border-blue-300 duration-300'
+            />
+            {suggestion.length > 0 && (
+              <div className='absolute top-12 left-0 right-0 bg-transparent shadow-md
+              rounded z-10'>
+              {suggestion.map((s)=>(
+                <button type='button' key={`${s.lat}-${s.lon}`}
+                onClick={() => fetchWeatherData(
+                  `//https://api.openweathermap.org/data/2.5/weather?lat=${s.lat}&lon=${s.lon}&appid={API_KEY}&units=metric`
+                  `${s.name}, ${s.country}${s.state ? `,${s.state}` : ' '}`
+                )} className='block hover:bg-blue-400 bg-transparent px-4 py-2 text-sm text-left w-full
+                transition-colors'>
+                {s.name},{s.country}{s.state `, ${s.state}`}
+                </button>
+                ))}
+              </div>
+            )}
+            <button type='submit' className='bg-purple-700 hover:bg-amber-400 text-white font-semibold
+            py-2 px-4 rounded transition-colors'> Get Weather</button>
+          </form>
+        ): (
+          <div className='mt-6 text-center transition-opacity duration-500'>
+             <button onClick={()=>{setWeather(null); setCity('')}}
+             className='mb-4 bg-violet-900 hover:bg-amber-700 text-white font-semibold
+             py-1 px-3 rounded transition-colors'>
+             Search Here
+             </button>
+          </div>
+        ) }
+
+      </div>
+     </div>
+
+    </div> 
   )
 }
 
---
+++ src/Component/WeatherBG.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import Thunderstorm_1 from "../assets/Thunderstorm_1.jpg"
+import Rain_1 from "../assets/Rain_1.jpg"
+import Rain_2 from "../assets/Rain_2.jpg"
+import SnowDay_1 from "../assets/SnowDay_1.jpg"
+import ClearDay_1 from "../assets/ClearDay_1.jpg"
+import ClearNight_1 from "../assets/ClearNight_1.jpg"
+import CloudsNight_1 from "../assets/CloudsNight_1.jpg"
+import CloudsDay_1 from "../assets/CloudsDay_1.jpg"
+import Haze_1 from "../assets/Haze_1.jpg"
+import bg_video_1 from "../assets/bg_video_1.mp4"
+
+
+const WeatherBG = ({ condition }) => {
+    const gifs = {
+        Thunderstorm_1,
+        Drizzle: Rain_1,
+        Rain_2,
+        Snow: SnowDay_1,
+        Clear: { day: ClearDay_1, night: ClearNight_1 },
+        Clouds: { day: CloudsDay_1, night: CloudsNight_1 },
+        Mist: Haze_1,
+        Smoke: Haze_1,
+        Haze_1,
+        Fog: Haze_1,
+        default: bg_video_1
+    };
+
+    const getBackground = () =>{
+        if(!condition) return gifs.default;
+        const weatherType =condition.main;
+        const asset = gifs[weatherType];
+
+        if(!asset) return gifs.default;
+        if(typeof async === 'object')
+            return condition.isDay ? asset.day : asset.night;
+        return asset;
+    }
+
+    const background = getBackground();
+
+    return(
+        <div className="fixed inset-0 z-0 overflow-hidden">
+            {background === bg_video_1 ? (
+                <video autoPlay loop muted  className='w-full h-full object-cover opacity-100
+                pointer-events-none animate-fade-in'>
+                    <source src={bg_video_1} type='video/mp4' />
+                </video>
+            ) : (
+             <img src={background} alt='Weather-bg' className='
+             w-full h-full object-cover opacity-20
+             pointer-events-none animate-fade-in'/>
+            )}
+
+            <div className='absolute inset-0 bg-black/30'/>
+        </div>
+    )
+}
+
+export default WeatherBG
